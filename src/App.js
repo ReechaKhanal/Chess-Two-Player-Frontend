c61f2c1b1d04089f0c12d9386a6df3896a9960e6
rkhanal <rkhanal@uno.edu>
2021-05-19 13:28:45 -0700

    Trying to overcome the timout error in heroku

--- src/App.js
+++ src/App.js
@@ -9,22 +9,34 @@ ReactDOM.render(
     <App />, document.getElementById('root')
 );
 */
-
+/*
 ReactDOM.render(
-  /*
+  
     <div class = "chess">
       <h1>Reecha's Chess Board</h1>
       <ChessBoard/>
     </div>,
     document.getElementById('root')
-  */
+  
     <div class = "chess">
       <h1>Reecha's Chess Board</h1>
       
     </div>
   );
 
+  export default App;
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+*/
+
+const App = () => {
+  return (
+    <div class = "chess">
+      <h1>Reecha's Chess Board</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/ChessBoard/chessBoard.js
+++ src/ChessBoard/chessBoard.js
@@ -14,10 +14,10 @@ import Identicon from 'react-identicons';
 
 
 //const client = new W3CWebSocket('ws://127.0.0.1:8000');
-//const client = new W3CWebSocket('ws://chess-two-player.herokuapp.com');
+const client = new W3CWebSocket('ws://chess-two-player.herokuapp.com');
 
 //const W3CWebSocket = require('websocket').w3cwebsocket;
-const client = new W3CWebSocket('https://chess-two-player.herokuapp.com');
+//const client = new W3CWebSocket('https://chess-two-player.herokuapp.com');
 
 class ChessBoard extends Component{
     /*
