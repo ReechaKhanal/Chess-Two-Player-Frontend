f5b2042844d0398a0d5bcbddb896a493f19ec174
rkhanal <rkhanal@uno.edu>
2021-05-19 14:11:23 -0700

    Trying to overcome the invalid webaddress issue in heroku

--- src/ChessBoard/chessBoard.js
+++ src/ChessBoard/chessBoard.js
@@ -17,7 +17,9 @@ import Identicon from 'react-identicons';
 //const client = new W3CWebSocket('chess-two-player.herokuapp.com');
 
 //const W3CWebSocket = require('websocket').w3cwebsocket;
-const client = new W3CWebSocket('https://chess-two-player.herokuapp.com');
+//const client = new W3CWebSocket('https://chess-two-player.herokuapp.com');
+
+const client = new W3CWebSocket('ws://chess-two-app-frontend.herokuapp.com/');
 
 class ChessBoard extends Component{
     /*
